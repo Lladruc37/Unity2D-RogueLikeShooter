dd0b1794722322b438e526eec7de2bca532b6739
Lladruc37 <sergi@kromatika.com>
2024-05-30 19:54:01 +0200

    Added some TODOs & space fixing

--- Assets/Scripts/General/GeneratorManager.cs
+++ Assets/Scripts/General/GeneratorManager.cs
@@ -5,79 +5,79 @@ using UnityEngine;
 [RequireComponent(typeof(GameManager))]
 public class GeneratorManager : MonoBehaviour
 {
-    public Map_Generation map_Generator;
+	public Map_Generation map_Generator;
 
-    private int roomsNow;
-    public int RoomsNow
-    {
-        get
-        {
-            return roomsNow;
-        }
-        set
-        {
-            if (value > 2 && value < maxRooms)
-            {
-                roomsNow = value;
-            }
-            else if (value < 3)
-            {
-                roomsNow = 3;
-            }
-            else if (value >= maxRooms)
-            {
-                roomsNow = maxRooms;
-            }
-        }
-    }
+	private int roomsNow;
+	public int RoomsNow
+	{
+		get
+		{
+			return roomsNow;
+		}
+		set
+		{
+			if (value > 2 && value < maxRooms)
+			{
+				roomsNow = value;
+			}
+			else if (value < 3)
+			{
+				roomsNow = 3;
+			}
+			else if (value >= maxRooms)
+			{
+				roomsNow = maxRooms;
+			}
+		}
+	}
 
-    public bool spawnEnemies;
+	public bool spawnEnemies;
 
-    public int roomIncreaseMin;
-    public int roomIncreaseMax;
+	public int roomIncreaseMin;
+	public int roomIncreaseMax;
 
-    public int minEnemies;
-    public int maxEnemies;
+	public int minEnemies;
+	public int maxEnemies;
 
-    public int maxRooms;
+	public int maxRooms;
 
-    public List<MapType> mapTypes;
+	public List<MapType> mapTypes;
 
-    private int playerSpawnRoom;
+	private int playerSpawnRoom;
 
-    private void Awake()
-    {
-        RoomsNow = 7;
-    }
+	private void Awake()
+	{
+		RoomsNow = 7;
+	}
 
-    public IEnumerator GenerateLevel(int rooms, bool spawnEnemies, UnityEngine.UI.Text text)
-    {
-        yield return new WaitForSecondsRealtime (3f);
-        RoomsNow = rooms;
-        this.spawnEnemies = spawnEnemies;
-        yield return map_Generator.Begin(roomsNow, text, true);
+	public IEnumerator GenerateLevel(int rooms, bool spawnEnemies, UnityEngine.UI.Text text)
+	{
+		yield return new WaitForSecondsRealtime(3f);
+		RoomsNow = rooms;
+		this.spawnEnemies = spawnEnemies;
+		yield return map_Generator.Begin(roomsNow, text, true);
 
-        Room playerRoom = null;
-        foreach (Room room in Map.roomObjs)
-        {
-            if (room.roomType == roomType.playerStart)
-            {
-                playerRoom = room;
-                break;
-            }
-        }
-        
-        playerSpawnRoom = playerRoom.number;
-        GameManager.instance.TeleportPlayer(playerRoom);
+		Room playerRoom = null;
+		foreach (Room room in Map.roomObjs)
+		{
+			if (room.roomType == roomType.playerStart)
+			{
+				playerRoom = room;
+				break;
+			}
+		}
 
-        if (spawnEnemies)
-        {
-            EnemiesManager.SpawnInit(playerSpawnRoom);
-        }
+		playerSpawnRoom = playerRoom.number;
+		GameManager.instance.TeleportPlayer(playerRoom);
 
-        if (text != null)
-        {
-            text.text = "";
-        }
-    }
+		if (spawnEnemies)
+		{
+			EnemiesManager.SpawnInit(playerSpawnRoom);
+		}
+
+		if (text != null)
+		{
+			text.text = "";
+		}
+	}
 }
